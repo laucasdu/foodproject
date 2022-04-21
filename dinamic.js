5202621c53257852bddb7e9f18c751655b4f24bd
EricPuig10 <puigvendrelloetona@gmail.com>
2022-04-21 15:25:17 +0200

    eric

---
+++ dinamic.js
@@ -0,0 +1,60 @@
+
+let variableNumero = 1;
+   
+    function sumar() {
+    variableNumero++
+    document.getElementById("number1").innerHTML = `${variableNumero}`;
+    }
+    
+    function restar() {
+        variableNumero--
+        document.getElementById("number1").innerHTML = `${variableNumero}`;
+    }
+
+let variableNumero2=1;
+    function sumar2() {
+    variableNumero2++
+    document.getElementById("number2").innerHTML = `${variableNumero2}`;
+    }
+    function restar2() {
+    variableNumero2--
+    document.getElementById("number2").innerHTML = `${variableNumero2}`;
+    }
+let variableNumero3=1;
+    function sumar3() {
+    variableNumero3++
+    document.getElementById("number3").innerHTML = `${variableNumero3}`;
+    }
+    function restar3() {
+        variableNumero3--
+        document.getElementById("number3").innerHTML = `${variableNumero3}`;}
+
+let variableNumero4=1;
+    function sumar4() {
+    variableNumero4++
+    document.getElementById("number4").innerHTML = `${variableNumero4}`;
+    }
+    function restar4() {
+        variableNumero4--
+        document.getElementById("number4").innerHTML = `${variableNumero4}`;}
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+   
+
+
+
